eebb63b9524908ddb30a913fb10ec25bcfb5e2ac
Hcdubsy <15789441+hcw001@users.noreply.github.com>
2025-02-17 11:37:49 -0500

    updated ignore

--- src/scripts/Global.lua
+++ src/scripts/Global.lua
@@ -8,7 +8,7 @@ end
 function onChat(message, sender)
     if message == 'setup' then
         print("Initializing...")
-        Global.call("setup_units")
+        Global.call("setup_units") --setup_units()
         print("Setup Complete.")
     end
 end
@@ -16,10 +16,23 @@ end
 function setup_units()
     local bag = getObjectFromGUID(UNITS[GERMANY][INF].guid)
     local position = POSITION.Greenland
-    bag.takeObject({
+    local takenObj = bag.takeObject({
         position = position,
         callback_function = function(taken_object)
             print("Place Infantry.")
         end
     })
+    -- takenObj.highlightOn('Blue')
 end
+
+-- function staggeredLoad(objects, index)
+--     index = index or 1
+--     if index > #objects then return end
+    
+--     local obj = objects[index]
+--     obj.setPositionSmooth({0,5,0})
+    
+--     Wait.time(function()
+--         staggeredLoad(objects, index+1)
+--     end, 0.2)
+-- end
